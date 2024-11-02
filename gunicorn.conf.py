83b8ed07f2bb05e66182ebf68de0774f89fa55e1
nyattoh <nyatto@gmail.com>
2024-11-02 14:18:20 +0900

    Add files via upload

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 # Server Socket
 bind = "0.0.0.0:10000"  # Renderのデフォルトポート
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = "sync"
 timeout = 300
 
