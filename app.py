0b4173723dfe90bdea2e1dea3612f3d14675a364
nyattoh <nyatto@gmail.com>
2024-11-05 00:55:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -217,5 +217,5 @@ if __name__ == '__main__':
         os.makedirs(app.config['SHARE_FOLDER'])
     
     # アプリケーションの実行
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
